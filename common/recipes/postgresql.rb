745fa3ee712f39c77705e8700fb67413ff710119
Yasuhisa Yamazaki <yasu@amrael.net>
2017-08-15 22:27:36 +0900

    Replace gdal package with conan custom binary package.

--- common/recipes/postgresql.rb
+++ common/recipes/postgresql.rb
@@ -1,6 +1,24 @@
 # install packages 
-%w{gdal proj-devel json-c postgresql95-devel}.each do |pkg|
+%w{proj-devel json-c postgresql95-devel}.each do |pkg|
   package pkg do
     action :upgrade
   end
+end
+
+# install gdal 2.1.3 using conan
+gdal_version = "2.1.3"
+gdal_hash = "de9c231f84c85def9df09875e1785a1319fa8cb6"
+
+bash 'install gdal' do
+  code <<-EOC
+  sudo pip install conan
+  conan remote add opsworks https://api.bintray.com/conan/trippiece/opsworks
+  conan install Gdal/#{gdal_version}@amrael/stable -r opsworks
+  export PATH=$PATH:$HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/#{gdal_hash}/bin
+sudo sh -c "echo 'export PATH=\$PATH:$HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/#{gdal_hash}/bin' > /etc/profile.d/gdal.sh"
+  sudo sh -c 'echo $HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/#{gdal_hash}/lib > /etc/ld.so.conf.d/gdal-#{gdal_version}.conf'
+  sudo ldconfig
+  EOC
+  user node[:app][:owner]
+  group node[:app][:group]
 end
