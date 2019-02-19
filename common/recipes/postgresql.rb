8c59d86693bfad5bfaf749714c2d1d61f65daf6e
Yasuhisa Yamazaki <yasu@amrael.net>
2019-02-19 13:00:33 +0900

    Support for Amazon linux 2018.03.

--- common/recipes/postgresql.rb
+++ common/recipes/postgresql.rb
@@ -7,7 +7,6 @@ end
 
 # install gdal 2.1.3 using conan
 gdal_version = "2.1.3"
-gdal_hash = "de9c231f84c85def9df09875e1785a1319fa8cb6"
 
 bash 'uninstall gdal and re-install its newer version with conan' do
   code <<-EOC
@@ -16,9 +15,10 @@ bash 'uninstall gdal and re-install its newer version with conan' do
   export PATH=$PATH:/usr/local/bin
   conan remote add opsworks https://api.bintray.com/conan/trippiece/opsworks
   conan install Gdal/#{gdal_version}@amrael/stable -r opsworks
-  sudo sh -c "echo 'export PATH=\$PATH:$HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/#{gdal_hash}/bin' > /etc/profile.d/gdal.sh"
-  export PATH=$PATH:$HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/#{gdal_hash}/bin
-  sudo sh -c "echo $HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/#{gdal_hash}/lib > /etc/ld.so.conf.d/gdal-#{gdal_version}.conf"
+  GDAL_HASH=`conan info Gdal/#{gdal_version}@amrael/stable | awk '/^[ \t]+ID:/ {print $2}'`
+  sudo sh -c "echo 'export PATH=\$PATH:$HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/$GDAL_HASH/bin' > /etc/profile.d/gdal.sh"
+  export PATH=$PATH:$HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/$GDAL_HASH/bin
+  sudo sh -c "echo $HOME/.conan/data/Gdal/#{gdal_version}/amrael/stable/package/$GDAL_HASH/lib > /etc/ld.so.conf.d/gdal-#{gdal_version}.conf"
   sudo ldconfig
   EOC
   user node[:app][:owner]
